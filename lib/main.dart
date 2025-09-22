a1181344d570ea8c5f7e585125778d13c644a604
이해준/전시DX지원파트 <back2basics@gsrpartner.com>
2025-09-22 17:00:58 +0900

    refactor: simplify dependency injection in main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:task02/core/network/dio_client.dart';
 import 'package:task02/data/datasource/post_remote_data_source_impl.dart';
 import 'package:task02/data/repositories/post_repository_impl.dart';
+import 'package:task02/domain/repositories/post_repository.dart';
 import 'package:task02/domain/usecases/get_posts_usecase.dart';
 import 'package:task02/domain/usecases/get_post_detail_usecase.dart';
 import 'package:task02/presentation/bloc/post_list_cubit.dart';
@@ -10,42 +11,39 @@ import 'package:task02/presentation/bloc/post_detail_cubit.dart';
 import 'package:task02/presentation/page/post_list_page.dart';
 
 void main() {
-  final dioClient = DioClient("https://jsonplaceholder.typicode.com");
-  final dataSource = PostRemoteDataSourceImpl(dioClient.dio);
-  final repository = PostRepositoryImpl(dataSource);
-  final getPostsUseCase = GetPostsUseCase(repository);
-  final getPostDetailUseCase = GetPostDetailUseCase(repository);
-
-  runApp(MyApp(
-    getPostsUseCase: getPostsUseCase,
-    getPostDetailUseCase: getPostDetailUseCase,
-  ));
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  final GetPostsUseCase getPostsUseCase;
-  final GetPostDetailUseCase getPostDetailUseCase;
-
   const MyApp({
     super.key,
-    required this.getPostsUseCase,
-    required this.getPostDetailUseCase,
   });
 
   @override
   Widget build(BuildContext context) {
-    return MultiBlocProvider(
-      providers: [
-        BlocProvider(
-          create: (_) => PostListCubit(getPostsUseCase: getPostsUseCase),
+    return RepositoryProvider<PostRepository>(
+      create: (_) => PostRepositoryImpl(
+        PostRemoteDataSourceImpl(
+          DioClient("https://jsonplaceholder.typicode.com").dio,
         ),
-        BlocProvider(
-          create: (_) => PostDetailCubit(getPostDetailUseCase: getPostDetailUseCase),
-        ),
-      ],
-      child: MaterialApp(
-        home: const PostListPage(),
       ),
+      child: Builder(builder: (context) {
+        return MultiBlocProvider(
+          providers: [
+            BlocProvider(
+              create: (_) =>
+                  PostListCubit(getPostsUseCase: GetPostsUseCase(RepositoryProvider.of<PostRepository>(context))),
+            ),
+            BlocProvider(
+              create: (_) => PostDetailCubit(
+                  getPostDetailUseCase: GetPostDetailUseCase(RepositoryProvider.of<PostRepository>(context))),
+            ),
+          ],
+          child: MaterialApp(
+            home: const PostListPage(),
+          ),
+        );
+      }),
     );
   }
 }
