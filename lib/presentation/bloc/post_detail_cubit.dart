5f9899438c4cbbf8c2c4ae767aefbb5537514fe5
이해준/전시DX지원파트 <back2basics@gsrpartner.com>
2025-09-22 17:25:18 +0900

    fix: handle DioException in PostDetailCubit and PostListCubit

--- lib/presentation/bloc/post_detail_cubit.dart
+++ lib/presentation/bloc/post_detail_cubit.dart
@@ -1,6 +1,8 @@
+import 'package:dio/dio.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:task02/presentation/bloc/post_detail_state.dart';
 
+import '../../core/app_exception.dart';
 import '../../domain/usecases/get_post_detail_usecase.dart';
 
 class PostDetailCubit extends Cubit<PostDetailState> {
@@ -13,6 +15,13 @@ class PostDetailCubit extends Cubit<PostDetailState> {
     try {
       final post = await getPostDetailUseCase(id);
       emit(PostDetailLoaded(post));
+    } on DioException catch (e) {
+      if (e.error is AppException) {
+        final appError = e.error as AppException;
+        emit(PostDetailError('${appError.code}: ${appError.message}'));
+      } else {
+        emit(PostDetailError(e.message ?? '알 수 없는 네트워크 오류'));
+      }
     } catch (e) {
       emit(PostDetailError(e.toString()));
     }
--- lib/presentation/bloc/post_list_cubit.dart
+++ lib/presentation/bloc/post_list_cubit.dart
@@ -1,4 +1,6 @@
+import 'package:dio/dio.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import '../../core/app_exception.dart';
 import 'post_list_state.dart';
 import '../../domain/usecases/get_posts_usecase.dart';
 
@@ -12,6 +14,13 @@ class PostListCubit extends Cubit<PostListState> {
     try {
       final posts = await getPostsUseCase();
       emit(PostListLoaded(posts));
+    } on DioException catch (e) {
+      if (e.error is AppException) {
+        final appError = e.error as AppException;
+        emit(PostListError('${appError.code}: ${appError.message}'));
+      } else {
+        emit(PostListError(e.message ?? '알 수 없는 네트워크 오류'));
+      }
     } catch (e) {
       emit(PostListError(e.toString()));
     }
